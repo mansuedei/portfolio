531993b8b1a3f959537f18beebd4eef6f62f857f
Yana Stepanova / mansuedei <mansuedei@yandex.ru>
2019-10-23 15:56:59 +0300

    added Font Awesome kit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 //все js модули создаем в папке scripts, и подключаем сюда
 import "./styles/main.pcss";
 if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
+	require("file-loader!./index.pug");
 }
 
 import "./scripts/skills";
+import "./scripts/FontAwesome";
---
+++ src/scripts/FontAwesome.js
@@ -0,0 +1,2 @@
+src = "https://kit.fontawesome.com/75defc7b9b.js";
+crossorigin = "anonymous"
